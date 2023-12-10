7bd2dbb76150c47ee2fe3f0d4cdf612c77738cda
mcli <mclimber28@gmail.com>
2023-12-10 10:22:26 -0400

    condicionales en los estilos

--- fazt-react-course/src/Task.js
+++ fazt-react-course/src/Task.js
@@ -1,6 +1,11 @@
-export function TaskCard() {
-    return <div>
+import './task.css'
+export function TaskCard({ready}) {
+    //const cardStyles = {background: '#202020', color: '#fff', padding: '20px'}
+    //const titleStyles = {fontWeight: "lighter"}
+    return <div className='card'>
         <h1>Mi primer tarea</h1>
-        <p>Tarea realizada</p>
+        <span style={ready ? {background: 'green'} : {background:'red'}}>
+            {ready ? "Tarea realizada" : "Tarea pendiente"}
+        </span>
     </div>
 }
--- fazt-react-course/src/index.js
+++ fazt-react-course/src/index.js
@@ -9,28 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-    <Button text="Click me"/>
-    <Button text="Pay" name='carolina'/>
-    <Button text='' name='Joe'/>
-    {/* <Greetting title="hola" name="li"/>
-    <Greetting title="hola" name="xavi"/>
-    <Greetting title="hola"/>
-    <UserCard 
-      name="Ryan Ray" 
-      amount={3000} 
-      married={true}
-      points={[99, 33.3, 22.2]}
-      address={{street: "123 main street", city: "New York"}}
-      greet={function(){alert('hello')}}
-    />
-    <UserCard 
-      name="Joe Mcmillan" 
-      amount={1000} 
-      married={false}
-      points={[100, 20]}
-      address={{street: "av some 123", city: "New York"}}
-      greet={function(){alert('hello')}}
-    />
-    <Product/> */}
+    <TaskCard ready={false}/>
   </>
 );
